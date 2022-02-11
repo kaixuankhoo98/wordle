38c0346db57c9d46561cbf1e78a63f0ba531aa81
Kaixuan Khoo <kaixuan.khoo.98@gmail.com>
2022-02-11 12:58:55 +0000

    generate word of the day function added

--- functions.py
+++ functions.py
@@ -1,4 +1,14 @@
 import numpy as np
+from datetime import datetime
+import random
+
+def generate_word_of_the_day():
+    file1 = "dictionaries/dictionary_allowed.txt"
+    dict_for_word_of_day = open_dictionary(file1)
+
+    date = datetime.now().year + datetime.now().month + datetime.now().day + 100
+    random.seed(date)
+    return dict_for_word_of_day[random.randint(0,len(dict_for_word_of_day))]
 
 def open_dictionary(filename):
     with open(filename, "r") as dict:
--- wordle.py
+++ wordle.py
@@ -2,8 +2,11 @@ from matplotlib.style import use
 import functions as f
 import numpy as np
 
-file = "dictionaries/toydict.txt"
-dict = f.open_dictionary(file)
-user_input = "icagr"
+word_of_the_day = f.generate_word_of_the_day()
+
+file = "dictionaries/dictionary_full.txt"
+full_dict = f.open_dictionary(file)
+
+user_input = "abate"
 # assert(f.is_user_input_in_dictionary(user_input,dict))
-print(f.check_word("cigar",user_input))
+print(f.check_word(word_of_the_day,user_input))
